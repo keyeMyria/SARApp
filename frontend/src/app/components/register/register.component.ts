5cbda5ce07eb6d2f31bd3edccf7e734d983c8cca
Richard Evaristo <richard120189@gmail.com>
2018-07-05 23:50:10 +0800

    configure signup component

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route : Router,
+    private http : HttpClient
+  ) { }
 
   ngOnInit() {
   }
+
+  public form = {
+
+    email: null,
+    name: null,
+    password: null,
+    password_confirmation: null
+
+  };
+
+  public error = [];
+
+  private baseUrl = 'http://localhost:8000/api/auth';
+
+  onSubmit() {
+
+    return this.http.post(`${this.baseUrl}/signup`,this.form).subscribe(
+
+      data => this.handleResponse(data),
+      error => this.handleError(error)
+
+    );
+
+  }
+
+  handleResponse(data) {
+
+    this.route.navigateByUrl('login');
+
+  }
+
+  handleError(error) {
+
+    this.error = error.error.errors;
+    
+  }
+
 
 }
