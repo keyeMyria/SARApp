95055d32afbdabd52fb301ae81c3c0ad801243d2
Richard Evaristo <richard120189@gmail.com>
2018-07-14 23:48:01 +0800

    fixed imports

--- frontend/src/app/shared/Services/auth.service.ts
+++ frontend/src/app/shared/Services/auth.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { TokenService } from './token.service';
-import { UserService } from './user.service';
-import { TimerService } from './timer.service';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,16 +11,11 @@ export class AuthService {
 
   authStatus = this.loggedIn.asObservable();
 
-
-  private timerActive = new BehaviorSubject<boolean>(this.Token.loggedIn());
-  timerStatus = this.timerActive.asObservable();
-
   changeAuthStatus(value : boolean){
     this.loggedIn.next(value);
-    this.timerActive.next(value);
+
   }
   constructor(
     private Token: TokenService, 
-    private Timer: TimerService
   ) { }
 }
